7379421bf86db56617b58811f8eb08a414632e81
toitek <issawandhe@gmail.com>
2022-07-01 04:15:27 +0300

    error fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,12 +4,12 @@ if __name__ == "__main__":
 
     nargs = len(sys.argv) - 1
     if nargs != 3:
-        print("Usage:./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     op = sys.argv[2]
     if op != "+" and op != '-' and op != '*' and op != '/':
-        print("Unnown operator. Available operators: +,-,* and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     from calculator_1 import add, sub, mul, div
