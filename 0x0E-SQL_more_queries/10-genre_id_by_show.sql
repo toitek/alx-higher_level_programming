d7f69b10e73b96ff64168dde66acd3a2f1c9a1af
toitek <issawandhe@gmail.com>
2022-09-14 14:49:03 +0300

    fixed an error

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT tv_shows.title ,  tv_show_genres  FROM hbtn_0d_tvshows.tv_shows JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id ORDER BY tv_shows.title ASC , tv_show_genres.genre_id ASC ;
+SELECT tv_shows.title ,  tv_show_genres.genre_id  FROM hbtn_0d_tvshows.tv_shows JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id ORDER BY tv_shows.title ASC , tv_show_genres.genre_id ASC ;
